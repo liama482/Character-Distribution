b8a927da1f06b717c7beb1baff6d581e99bfda37
liama482 <liamabbate@hanovernorwichschools.org>
2016-10-06 13:36:27 -0400

    Updated from Brython Server: 10/6/2016 1:36:24 PM

--- distribution.py
+++ distribution.py
@@ -49,12 +49,13 @@ wappee = []
 def compare(a, b):
     if b[1]<a[1]: #comparing numerical 2nd values in each tuple
             return True
+    elif b[1]==a[1]:
+        if b[0]>a[0]: #comparing string 1st values in each tuple
+            return True
+        else:
+            return False
     else:
         return False
-    #    if b[0]>a[0]: #comparing string 1st values in each tuple
-    #        return True
-    #    else:
-    #        return False
 
 def bsort(seq, cmp):
     """
